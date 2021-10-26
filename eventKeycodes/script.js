8be43d601960cf1272997ce2a7c57cce2cc9f649
Tourscholar <krj1608178365@gmail.com>
2021-10-27 00:09:45 +0900

    event keycodes

---
+++ eventKeycodes/script.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (event) => {
+  // console.log(event);
+  insert.innerHTML = `<div class="key">
+    <! --解决空格显示bug -->
+    ${event.key === " " ? "Space" : event.key}
+    <small>event.key</small>
+    </div>
+    <div class="key">
+      ${event.keyCode}
+      <small>event.keyCode</small>
+    </div>
+    <div class="key">
+      ${event.code}
+      <small>event.Code</small>
+    </div>`;
+});
