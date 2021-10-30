eaae5c8bfadde0b52247d9911f87077220e305e4
Tourscholar <krj1608178365@gmail.com>
2021-10-31 00:52:53 +0900

    random choice picker update

--- randomChoicePicker/script.js
+++ randomChoicePicker/script.js
@@ -4,6 +4,7 @@ const tagsEl = document.getElementById("tags");
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
   if (e.key === "Enter") {
+    /* 按下回车后将输入框清空 */
     e.target.value = "";
     randomSelect();
   }
@@ -11,37 +12,55 @@ textarea.addEventListener("keyup", (e) => {
 
 function createTags(input) {
   const tags = input
+    /* split函数将输入的字符串分割为字符数组 */
     .split(", ")
+    /* trim函数清除数组头尾的空格 */
     .map((tag) => tag.trim())
+    /* filter函数当数组不为空时返回数组内容 */
     .filter((tag) => tag !== "");
+  /* 先清空所有sapn标签 */
   tagsEl.innerHTML = "";
   tags.forEach((tag) => {
+    /* 创建span标签 */
     const tagEl = document.createElement("span");
+    /* 给span标签加上tag类名 */
     tagEl.classList.add("tag");
+    /* 将文本传入span标签 */
     tagEl.innerText = tag;
+    /* 将span标签挂载到tags下 */
     tagsEl.appendChild(tagEl);
   });
 }
 
 function randomSelect() {
-  const tags = document.querySelectorAll(".tag");
   const interval = setInterval(() => {
     const randomTag = pickRandomSelect();
     if (randomTag !== undefined) {
+      /* 设置高亮 */
       highlightTag(randomTag);
       setTimeout(() => {
+        /* 取消高亮 */
         unhighlight(randomTag);
       }, 100);
     }
   }, 100);
-  const times = tags.length;
+  /* 设置随机次数 */
+  const tags = document.querySelectorAll(".tag");
+  const times = tags.length * 10;
   setTimeout(() => {
     clearInterval(interval);
-  }, times * 1000);
+    setTimeout(() => {
+      const randomTag = pickRandomSelect();
+      if (randomTag !== undefined) {
+        highlightTag(randomTag);
+      }
+    }, 100);
+  }, times * 100);
 }
 
 function pickRandomSelect() {
   const tags = document.querySelectorAll(".tag");
+  /* 返回[0, span标签长度) */
   return tags[parseInt(Math.random() * tags.length)];
 }
 
